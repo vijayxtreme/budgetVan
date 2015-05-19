aeea07e54b5def01731ddcb5bd86865577800aa3
Vijay Menon <vijaymenonx@gmail.com>
2015-05-18 18:47:41 -0700

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt){
+//jshint, concat, uglify, sass
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all:['js/*.js']
+		},
+		clean:['public', 'public'],
+		concat: {
+				index:{
+					src:['index.html'],
+					dest:'public/index.html'
+				},
+				js: {
+					src: ['js/*.js'],
+					dest: 'public/production.js'
+				}
+		},
+		sass: {
+			dist: {
+				files:[{
+					expand: true, //specify a directory
+					cwd: 'styles', //current working directory
+					src: ['*.scss'],
+					dest: 'public',
+					ext: '.css'
+				}]
+			}
+		},
+		uglify: {
+			build:{
+				src:'public/*.js',
+				dest: 'public/production.min.js'
+			}
+		}
+
+	});
+
+	//Load files
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.registerTask('default', ['jshint','clean', 'concat', 'sass', 'uglify']);
+};
