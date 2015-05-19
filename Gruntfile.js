1170212ee778f0a8a5dc9aacc55b2a4447883dff
Vijay Menon <vijaymenonx@gmail.com>
2015-05-19 15:14:36 -0700

    Cleaned files, minified, created public folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,20 +23,37 @@ module.exports = function(grunt){
 				}]
 			}
 		},
-		uglify: {
-			build:{
-				src:'styles/*.css',
-				dest: 'public/style.min.css'
+		imagemin: {
+			dynamic:{
+				files:[{
+					expand:true,
+					cwd: '.',
+					src: ['img/*.{png,jpg}'],
+					dest:'public/'
+				}]
 			}
+		},
+		cssmin: {
+		  target: {
+		    files: [{
+		      expand: true,
+		      cwd: 'styles',
+		      src: ['*.css', '!*.min.css'],
+		      dest: 'public/styles/',
+		      ext: '.min.css'
+		    }]
+		  }
 		}
 
 	});
 
 	//Load files
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.registerTask('default', ['clean', 'concat', 'sass']);
+	grunt.registerTask('default', ['clean', 'concat', 'sass', 'imagemin','cssmin']);
 };
